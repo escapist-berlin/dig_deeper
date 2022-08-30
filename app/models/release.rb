98e6960f01d4631f07719682b62936f17b9fb927
dk <denis.kolokolov@icloud.com>
2022-08-30 15:04:26 +0200

    adding release model

---
+++ app/models/release.rb
@@ -0,0 +1,3 @@
+class Release < ApplicationRecord
+  belongs_to :list
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_30_125012) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_30_130149) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "lists", force: :cascade do |t|
+    t.string "title"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_lists_on_user_id"
+  end
+
+  create_table "releases", force: :cascade do |t|
+    t.string "artist"
+    t.string "title"
+    t.string "label"
+    t.string "catalog_number"
+    t.string "format"
+    t.date "released"
+    t.string "styles"
+    t.string "tracklist"
+    t.string "favorite_tracks"
+    t.string "tag"
+    t.string "link"
+    t.string "comment"
+    t.bigint "list_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["list_id"], name: "index_releases_on_list_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +53,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_30_125012) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "lists", "users"
+  add_foreign_key "releases", "lists"
 end
