03c19dc8ee3fa4ca9da8a3db8c371b1d716e5727
dk <denis.kolokolov@icloud.com>
2022-08-31 14:14:13 +0200

    API setup

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,26 @@ class PagesController < ApplicationController
   end
 
   def dashboard
+    @search = params['search']
+    if @search.present?
+      @search_query = @search['title']
+    end
   end
+
+  def results
+    @results = discogs_api(@search_query)
+  end
+
+  private
+
+  def discogs_api(search_input)
+    wrapper = Discogs::Wrapper.new('DigDeeper')
+    auth_wrapper = Discogs::Wrapper.new('DigDeeper', user_token: 'woLJMkIArTDEQgXUMPAAuNacoDrNRjoZRUVoVZLU')
+
+    search = auth_wrapper.search(search_input, :per_page => 1)
+    release_id = search.results.first.id.to_s
+    release = wrapper.get_release(release_id)
+    return release
+  end
+
 end
