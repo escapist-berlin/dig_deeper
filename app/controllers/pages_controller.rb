ac193748df2bf1e5a0d764835519588f234c7425
dk <denis.kolokolov@icloud.com>
2022-08-31 15:49:08 +0200

    api search works

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,14 +3,12 @@ class PagesController < ApplicationController
   end
 
   def dashboard
-    @search = params['search']
-    if @search.present?
-      @search_query = @search['title']
-    end
   end
 
   def results
-    @results = discogs_api(@search_query)
+    @search_query = params['search']['title']
+    @release = discogs_api(@search_query)
+    @cover_image = discogs_api_img(@search_query)
   end
 
   private
@@ -21,8 +19,14 @@ class PagesController < ApplicationController
 
     search = auth_wrapper.search(search_input, :per_page => 1)
     release_id = search.results.first.id.to_s
-    release = wrapper.get_release(release_id)
-    return release
+    wrapper.get_release(release_id)
   end
 
+  def discogs_api_img(search_input)
+    wrapper = Discogs::Wrapper.new('DigDeeper')
+    auth_wrapper = Discogs::Wrapper.new('DigDeeper', user_token: 'woLJMkIArTDEQgXUMPAAuNacoDrNRjoZRUVoVZLU')
+
+    search = auth_wrapper.search(search_input, :per_page => 1)
+    search.results.first.cover_image
+  end
 end
